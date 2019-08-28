31705c28e0ea10923c852b1535dd4607430fe860
ansonchaan <ansonchan9523@gmail.com>
2019-08-28 16:09:05 +0800

    updates

--- src/Dropping3d/index.js
+++ src/Dropping3d/index.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import * as THREE from 'three';
 import Orbitcontrols from 'three-orbitcontrols';
 import Ammo from 'ammo.js';
-// import CANNON from 'cannon';
-// import CannonDebugRenderer from './CannonDebugRenderer';
 
 const Dropping3d = (props) => {
     const [sceneElem, setSceneElem] = useState(null);
@@ -22,15 +20,17 @@ const Dropping3d = (props) => {
             // let groundCM;
             const startTime = new Date();
             let collisionConfiguration;
-			let dispatcher;
-			let broadphase;
-			let solver;
-            let physicsWorld,
+			let dispatcher,
+			    broadphase,
+			    solver,
+                physicsWorld,
                 transformAux1;
-            // let cannonDebugRenderer;
+            const cameraDepth = 30;
+            let addObjectTime = new Date(),
+                addForceTime = new Date();
 
             const initScene = () => {
-                camera = new THREE.OrthographicCamera( width / - 30, width / 30, height / 30, height / - 30, 0.1, 1000 );
+                camera = new THREE.OrthographicCamera( width / - cameraDepth, width / cameraDepth, height / cameraDepth, height / - cameraDepth, 0.1, 1000 );
                 // camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
                 camera.position.x = 50;
@@ -53,8 +53,6 @@ const Dropping3d = (props) => {
                 initLights();
                 initPhysics();
                 initGeometry();
-
-                // cannonDebugRenderer = new CannonDebugRenderer(scene, world);
             }
 
             const initPhysics = () => {
@@ -74,7 +72,7 @@ const Dropping3d = (props) => {
                 scene.add(ambientLight);
 
                 const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-                pointLight.position.set( 20, 20, 0 );
+                pointLight.position.set( 20, 50, 0 );
                 pointLight.castShadow = true;
                 pointLight.shadow.mapSize.width = 2048;
                 pointLight.shadow.mapSize.height = 2048;
@@ -82,11 +80,7 @@ const Dropping3d = (props) => {
             }
 
             const initGeometry = () => {
-                addGround();
-
-                for(let i=0; i<numOfBoxes; i++){
-                    addObject(i);
-                }
+                createContainer();
             }
 
             const addObject = (i = Math.round(Math.random()*4)) => {
@@ -99,31 +93,37 @@ const Dropping3d = (props) => {
                     addMesh(i,'cone');
             }
 
-            const createRigidBody = (threeObject, physicsShape, mass, pos, quat) => {
-                var transform = new Ammo.btTransform();
+            const createRigidBody = (threeObject, physicsShape, mass, pos, quat, visible = true) => {
+                const transform = new Ammo.btTransform();
 				transform.setIdentity();
 				transform.setOrigin( new Ammo.btVector3( pos.x, pos.y, pos.z ) );
 				transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
-                var motionState = new Ammo.btDefaultMotionState( transform );
+                const motionState = new Ammo.btDefaultMotionState( transform );
                 
-                var localInertia = new Ammo.btVector3( 0, 0, 0 );
+                const localInertia = new Ammo.btVector3( 0, 0, 0 );
                 physicsShape.calculateLocalInertia( mass, localInertia );
                 
-                var rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, physicsShape, localInertia );
-                var body = new Ammo.btRigidBody( rbInfo );
+                const rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, physicsShape, localInertia );
+                const body = new Ammo.btRigidBody( rbInfo );
                 
                 threeObject.userData.physicsBody = body;
 
                 if(mass > 0){
-					bodies.push( threeObject );
-
+                    bodies.push( threeObject );
+                
 					// Disable deactivation
 					body.setActivationState( 4 );
-				}
+                }
+                
+                body.setRestitution(.7);
+                body.setDamping(0.05,0);
 
-                scene.add( threeObject );
+
+                if(visible)
+                    scene.add( threeObject );
+
                 physicsWorld.addRigidBody( body );
-console.log(physicsWorld);
+
                 return body;
             }
 
@@ -132,9 +132,9 @@ console.log(physicsWorld);
                 let size;
                 let geometry;
                 let shape;
-                const x = Math.round(Math.random()*20 -10);
-                const y = 5;
-                const z = Math.round(Math.random()*20 -10);
+                const x = Math.round(Math.random()*15 - 8);
+                const y = 20;
+                const z = Math.round(Math.random()*15 - 8);
 
                 if(type === 'sphere'){
                     size = 2;
@@ -156,100 +156,84 @@ console.log(physicsWorld);
 
                 const material = new THREE.MeshPhongMaterial({ color: colors[i%5] });
                 const mesh = new THREE.Mesh( geometry, material );
-                mesh.position.x = x;
-                mesh.position.y = 10;
-                mesh.position.z = z;
+                mesh.position.set(x, y, z);
+                mesh.rotation.set(Math.random()* 360 * Math.PI/180, Math.random()* 360 * Math.PI/180, Math.random()* 360 * Math.PI/180);
                 mesh.castShadow = true;
                 mesh.receiveShadow = true;
 
                 
-                
                 createRigidBody(mesh, shape, 1, mesh.position, mesh.quaternion);
-
-                // bodies.push(mesh);
-                // objPhys.push(boxBody);
             }
-            
-            // const addSphere = (i) => {
-            //     const size = 2;
-            //     const x = Math.round(Math.random()*20 -10);
-            //     const y = 20;
-            //     const z = Math.round(Math.random()*20 -10);
-            //     const geometry = new THREE.SphereGeometry( size, 32, 32 );
-            //     const material = new THREE.MeshPhongMaterial({ color: colors[i%5] });
-            //     const sphere = new THREE.Mesh( geometry, material );
-            //     sphere.castShadow = true;
-            //     sphere.receiveShadow = true;
-            //     scene.add(sphere);
 
-            //     const sphereCM = new CANNON.Material();
-            //     const sphereShape = new CANNON.Sphere(size);
-            //     const sphereBody = new CANNON.Body({ 
-            //         mass: 5, 
-            //         shape: sphereShape,
-            //         position: new CANNON.Vec3(x, y, z),
-            //         material: sphereCM
-            //     });
-            //     world.add( sphereBody );
-
-            //     const sphereGroundContact = new CANNON.ContactMaterial(groundCM, sphereCM, {
-            //         friction: 0.5,
-            //         restitution: 0.5 
-            //     });
-            //     world.addContactMaterial(sphereGroundContact);
-                
-
-            //     object.push(sphereBody);
-            //     bodies.push(sphere);
-            // }
-
-            const addGround = () => {
-                const size = 20;
+            const addWall = (size, pos, quat, visible) => {
                 const geometry = new THREE.BoxGeometry(size, size, 1);
                 const material = new THREE.MeshPhongMaterial({ color: new THREE.Color(bgColor) });
                 const plane = new THREE.Mesh( geometry, material );
-                // plane.position.y = -15;
-                plane.rotation.x = -90 * (Math.PI/180);
-                plane.castShadow = true;
-                plane.receiveShadow = true;
-                scene.add(plane);
+                plane.position.copy(pos);
+                plane.rotation.set(quat.x, quat.y, quat.z);
+                if(visible){
+                    plane.castShadow = true;
+                    plane.receiveShadow = true;
+                }
 
 
                 const shape = new Ammo.btBoxShape( new Ammo.btVector3( size * 0.5, size * 0.5, 1 * 0.5 ) );
                 shape.setMargin( 0.05 );
-                createRigidBody(plane, shape, 0, plane.position, plane.quaternion);
+                createRigidBody(plane, shape, 0, plane.position, plane.quaternion, visible);
+            }
+
+            const createContainer = () => {
+                const size = 20;
+                addWall(size, new THREE.Vector3(0, 0, 0), new THREE.Vector3(-90 * (Math.PI/180), 0, 0)); // ground
+                // addWall(size, new THREE.Vector3(-size/2, size/2, 0), new THREE.Vector3(0, -90 * (Math.PI/180), 0), false); // left
+                // addWall(size, new THREE.Vector3(size/2, size/2, 0), new THREE.Vector3(0, -90 * (Math.PI/180), 0), false); // right
+                // addWall(size, new THREE.Vector3(0, size/2, size/2), new THREE.Vector3(0, 0, 0), false); // front
+                // addWall(size, new THREE.Vector3(0, size/2, -size/2), new THREE.Vector3(0, 0, 0), false); // back
+            }
+
+            const addForce = () => {
+                const tbv30 = new Ammo.btVector3();
+                for (var i = 0, lth = bodies.length; i < lth; i++){
+                    tbv30.setValue(Math.random()*10-5,Math.random()*5+10,Math.random()*10-5);
+                    bodies[i].userData.physicsBody.setLinearVelocity(tbv30);
+                }
             }
 
             const update = () => {
-                const timer = (new Date() - startTime) * 0.1;
+                const timer = (new Date() - startTime) * 0.01;
                 // Step world
                 physicsWorld.stepSimulation( timer, 10 );
                 
+                if(new Date() - addObjectTime >= .2*1000) {
+                    addObject();
+                    addObjectTime = new Date();
+                }
 
-                // cannonDebugRenderer.update();
-                // if(Math.round(timer%9) === 0)
-                //     addObject();
+                if(new Date() - addForceTime >= 5*1000) {
+                    addForce();
+                    addForceTime = new Date();
+                }
 
                 for (var i = 0, lth = bodies.length; i < lth; i++){
                     const objThree = bodies[i];
-                    const objPhys = objThree.userData.physicsBody;
-                    const ms = objPhys.getMotionState();
-                    if(ms){
-                        ms.getWorldTransform( transformAux1 );
-						var p = transformAux1.getOrigin();
-						var q = transformAux1.getRotation();
-                        objThree.position.set(p.x(), p.y(), p.z());
-                        objThree.quaternion.set(q.x(), q.y(), q.z(), q.w());
+                    if(objThree){
+                        const objPhys = objThree.userData.physicsBody;
+                        const ms = objPhys.getMotionState();
+                        if(ms){
+                            ms.getWorldTransform( transformAux1 );
+                            var p = transformAux1.getOrigin();
+                            var q = transformAux1.getRotation();
+                            objThree.position.set(p.x(), p.y(), p.z());
+                            objThree.quaternion.set(q.x(), q.y(), q.z(), q.w());
 
-                //         if(b.position.y < -20){
-                //             world.remove(b);
-                //             object.splice(i,1);
-
-                //             scene.remove(m);
-                //             objThree.splice(i,1);
-
-                //             world.contactmaterials.splice(i,1);
-                //         }
+                            if(objThree.position.y < -20){
+                                physicsWorld.removeRigidBody(objPhys);
+                                objThree.geometry.dispose();
+                                objThree.material.dispose();
+                                scene.remove(objThree);
+                                bodies.splice(i,1);
+                            }
+                        }
                     }
                 }
             }
@@ -260,17 +244,18 @@ console.log(physicsWorld);
             }
 
             document.addEventListener('click',()=>{            
-                addObject();
+                // addObject();
+                
             });
             document.addEventListener('touchstart',()=>{            
                 addObject();
             });
 
             onWindowResize = () => {
-                camera.left = -window.innerWidth / 30;
-                camera.right = window.innerWidth / 30;
-                camera.top = window.innerHeight / 30;
-                camera.bottom = -window.innerHeight / 30;
+                camera.left = -window.innerWidth / cameraDepth;
+                camera.right = window.innerWidth / cameraDepth;
+                camera.top = window.innerHeight / cameraDepth;
+                camera.bottom = -window.innerHeight / cameraDepth;
                 // camera.aspect = window.innerWidth / window.innerHeight;
 				camera.updateProjectionMatrix();
 				renderer.setSize( window.innerWidth, window.innerHeight );

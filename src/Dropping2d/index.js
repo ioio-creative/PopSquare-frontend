f4d99f14ecfb75347ba94d94a2101f94e0cf9df7
ansonchaan <ansonchan9523@gmail.com>
2020-02-04 20:53:06 +0800

    add loading scene

--- src/Dropping2d/index.js
+++ src/Dropping2d/index.js
@@ -5,12 +5,16 @@ import LoadingScene from './LoadingScene';
 // import webSocket from 'socket.io-client';
 // import { Redirect } from 'react-router-dom';
 import * as PIXI from 'pixi.js';
+import './style.scss';
 import decomp from 'poly-decomp';
 window.decomp = decomp;
 
 const Dropping2d = (props) => {
     const sceneElem = useRef(null);
     const tempSceneElem = useRef(null);
+    const pick = useRef(null);
+    const up = useRef(null);
+    const bg = useRef(null);
     // const [socket,setSocket] = useState(null);
 
 
@@ -51,7 +55,9 @@ const Dropping2d = (props) => {
         let eyesArray = [];
         const shapes = [];
         const colors = ['3e5bb7', 'fa8b43', '498e8b', 'ea7da4'];
+        let started = false;
         let timeScaleTarget = 1;
+        let group = -1;
         const images = [
             {pID:1, src:'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg'}
         ]
@@ -71,7 +77,7 @@ const Dropping2d = (props) => {
         const ww = window.innerWidth,
             wh = window.innerHeight;
 
-        let seconds = 30;
+        let seconds = 5;
         const end = new Date();
         end.setSeconds(end.getSeconds() + seconds + 1);
 
@@ -93,7 +99,7 @@ const Dropping2d = (props) => {
         });
 
         const addWalls = () => {
-            const params = { isStatic: true, restitution: .8 };
+            const params = { isStatic: true, restitution: .8, collisionFilter: { group: group } };
             const top = Bodies.rectangle(ww/2,-30, ww, 60, { ...params });
             const wallLeft = Bodies.rectangle(-30, wh/2, 60, wh, { ...params });
             const wallRight = Bodies.rectangle(ww+30, wh/2, 60, wh, { ...params });
@@ -111,7 +117,7 @@ const Dropping2d = (props) => {
                 mouse: mouse,
                 constraint: {
                     stiffness: 0.2,
-                    render: {   
+                    render: {
                         visible: false
                     }
                 }
@@ -121,72 +127,103 @@ const Dropping2d = (props) => {
 
 
 
-        // create body
-        const createObject = () => {
+
+        const createObject = (isloadingObject = false) => {
+            const num = Math.round(Math.random() * 2);
+            const id = Math.round(Math.random()*2);
+            let _obj = null;
+
+            if(num === 0){
+                _obj = createShape('circle', isloadingObject);
+            }
+            else if(num === 1){
+                _obj = createShape('triangle', isloadingObject);
+            }
+            else if(num === 2){
+                _obj = createShape('halfCircle', isloadingObject);
+            }
+
+            _obj.productID = id;
+
+            objects.push(_obj);
+            if(objects.length>4) removeObject(0);
+
+            World.add(engine.world, _obj);
+        };
+
+        const createShape = (shape, isloadingObject) => {
             const x = Math.max(ww*.2, Math.min(ww*.8, Math.random() * ww));
             const y = 100;
             const radius = Math.round(Math.random() * 30 + 70);
-            const num = Math.round(Math.random() * 2);
-            const params = { restitution: .8 };
-            let newobj = null;
+            const params = { restitution: .8, collisionFilter: { group: 0 } };
             const r = radius * 2;
+            let body = null;
+            let graphics = null;
 
-            if(num === 0){
-                newobj = Bodies.circle(x, y, r, {...params });
-                createCircle(r);
+            switch(shape){
+                case 'circle':
+                    body = Bodies.circle(x, y, r, {...params });
+                    graphics = createCircleGraphics(shape, r);
+                    break;
+                    
+                case 'triangle':
+                    const vtx = [
+                        [r,0],
+                        [r*2,r*2],
+                        [0, r*2]
+                    ]
+                    const path = [].concat(vtx[0],vtx[1],vtx[2]);
+                    const triangle = Vertices.fromPath(path.join(' '));
+                    body = Bodies.fromVertices(x, y, Common.choose([triangle]), {...params}, true);
+                    graphics = createTriangleGraphics(shape, path);
+                    break;
+
+                case 'halfCircle':
+                    body = Bodies.rectangle(x, y, r*2, r, { 
+                        chamfer: { radius: [r*.99,r*.99,0] },
+                        ...params
+                    });
+                    graphics = createHalfCircleGraphics(shape, r);
+                    break;
+
+                default:
+                    break;
             }
-            else if(num === 1){
-                const vtx = [
-                    [r,0],
-                    [r*2,r*2],
-                    [0, r*2]
-                ]
-                const path = [].concat(vtx[0],vtx[1],vtx[2]);
-                const triangle = Vertices.fromPath(path.join(' '));
-                newobj = Bodies.fromVertices(x, y, Common.choose([triangle]), {...params}, true);
-                createTriangle(path);
-            }
-            else if(num === 2){
-                newobj = Bodies.rectangle(x, y, r*2, r, { 
-                    chamfer: { radius: [r*.99,r*.99,0] },
-                    ...params
-                });
-                createHalfCircle(r);
-            }
+            
 
-            const id = Math.round(Math.random()*2);
-            newobj.productID = id;
+            if(isloadingObject)
+                createGraphicWithShadow(graphics);
+            else
+                createGraphicWithInfo(graphics);
 
-            objects.push(newobj);
-            if(objects.length>4) removeObject(0);
+            return body;
+        }
 
-            World.add(engine.world, newobj);
-        };
-
-        const createCircle = (radius) => {
+        const createCircleGraphics = (shape, radius) => {
             const color = colors[0];
             const graphics = new PIXI.Graphics();
             graphics.beginFill(`0x${color}`, 1);
             graphics.drawCircle(0, 0, radius);
             graphics.endFill();
-            graphics.name = 'circle';
+            graphics.name = shape;
 
-            createGraphic(graphics);
+            return graphics;
         }
 
-        const createHalfCircle = (radius) => {
+        const createHalfCircleGraphics = (shape, radius) => {
             const color = colors[1];
             const graphics = new PIXI.Graphics();
             graphics.beginFill(`0x${color}`, 1);
             graphics.arc(0, 0, radius, 1 * Math.PI, 2 * Math.PI);
             graphics.endFill();
             graphics.pivot.y = -(radius/2 - radius*.075);
-            graphics.name = 'halfcircle';
+            graphics.name = shape;
 
-            createGraphic(graphics);
+            return graphics;
+            // createGraphic(graphics);
         }
 
-        const createTriangle = (vtx) => {
+        const createTriangleGraphics = (shape, vtx) => {
             const color = colors[2];
             const graphics = new PIXI.Graphics();
             graphics.beginFill(`0x${color}`, 1);
@@ -202,18 +239,41 @@ const Dropping2d = (props) => {
             graphics.pivot.x = graphics.width/2;
             graphics.pivot.y = graphics.height/1.5;
 
-            graphics.name = 'triangle';
+            graphics.name = shape;
 
-            createGraphic(graphics);
+            return graphics;
+        }
+
+        const createGraphicWithShadow = (graphics) => {
+            const container = new PIXI.Container();
+            const graphicsContainer = new PIXI.Container();
+            const shadow = graphics.clone();
+            shadow.tint = 0x333333;
+            shadow.pivot.x = graphics.pivot.x;
+            shadow.pivot.y = graphics.pivot.y;
+            shadow.x = -20;
+            shadow.y = 15;
+            
+            graphicsContainer.addChild(shadow);
+            graphicsContainer.addChild(graphics);
+            createEyes(graphicsContainer);
+            
+            container.addChild(graphicsContainer);
+            app.stage.addChild(container);
+
+            // store data
+            graphicsArray.push(graphicsContainer);
+            detailsArray.push(null);
+            shapes.push(container);
         }
         
-        const createGraphic = (graphics) => {
+        const createGraphicWithInfo = (graphics) => {
             const container = new PIXI.Container();
             const graphicsContainer = new PIXI.Container();
             const detailsContainer = new PIXI.Container();
             const size = graphics.width*.1;
             const cartName = 'Cart1';
-            const productName = '';
+            const productName = 'Product\nName';
             const productID = 1;
             let tempgraphics = graphics.clone();
             tempgraphics.name = graphics.name;
@@ -233,7 +293,6 @@ const Dropping2d = (props) => {
             graphicsArray.push(graphicsContainer);
             detailsArray.push(detailsContainer);
             shapes.push(container);
-
 
             gsap.set(graphics.scale, {x:0, y:0});
             gsap.to(graphics.scale, .6, {x:1, y:1, ease:'elastic.out(1, 0.6)'});
@@ -322,7 +381,7 @@ const Dropping2d = (props) => {
                 fontWeight: "bold",
                 letterSpacing: 1
             });
-            const text = new PIXI.Text('Product\nName', style);
+            const text = new PIXI.Text(productName, style);
             text.pivot.x = text.width/2;
             text.pivot.y = text.height/2;
             // text.alpha = 0;
@@ -362,6 +421,22 @@ const Dropping2d = (props) => {
             }
 
             container.addChild(image);
+        }
+        
+
+        const showProductDetails = () => {
+            for(let i=0; i<detailsArray.length; i++){
+                const detail = detailsArray[i];
+                const text = detail.children[0];
+                const image = detail.children[1];
+                
+                gsap.to(eyesArray, .3, {alpha:0, overwrite:true, ease:'power3.inOut'});
+                gsap.to(detailsArray, .3, {alpha:1, overwrite:true, ease:'power3.inOut'});
+
+                const tl = gsap.timeline({delay:Math.random()*6+2, repeat:-1, repeatDelay:2, yoyo:true});
+                tl.to(text, .6, {alpha:1, ease:'power3.inOut'});
+                tl.to(image, .6, {alpha:1, ease:'power3.inOut'},3);
+            }
         }
 
         const removeSpecificObject = (pID) => {
@@ -414,9 +489,10 @@ const Dropping2d = (props) => {
             while(graphicsArray[i].children[0]){
                 graphicsArray[i].removeChild(graphicsArray[i].children[0])
             }
-            while(detailsArray[i].children[0]){
-                detailsArray[i].removeChild(detailsArray[i].children[0])
-            }
+            if(detailsArray[i])
+                while(detailsArray[i].children[0]){
+                    detailsArray[i].removeChild(detailsArray[i].children[0])
+                }
             while(shapes[i].children[0]){
                 shapes[i].removeChild(shapes[i].children[0])
             }
@@ -434,27 +510,44 @@ const Dropping2d = (props) => {
 
             // console.log(seconds);
             if(seconds === 0){
-                console.log('end');
-                explosion();
-                showProductDetails();
+                if(started){
+                    explosion();
+                    showProductDetails();
+                }
+                else{
+                    // ranking
+                }
             }
         }
 
-        const showProductDetails = () => {
-            for(let i=0; i<detailsArray.length; i++){
-                const detail = detailsArray[i];
-                const text = detail.children[0];
-                const image = detail.children[1];
-                
-                gsap.to(eyesArray, .3, {alpha:0, overwrite:true, ease:'power3.inOut'});
-                gsap.to(detailsArray, .3, {alpha:1, overwrite:true, ease:'power3.inOut'});
+        let doOnce = false;
+        let idx = 0;
+        let by = 0;
+        let counter = 0;
+        Events.on(engine, 'beforeUpdate', function(event) {
+            const timer = performance.now()/1000;
+            counter += 1;
 
-                const tl = gsap.timeline({delay:2, repeat:-1, repeatDelay:2, yoyo:true});
-                tl.to(text, .6, {alpha:1, ease:'power3.inOut'});
-                tl.to(image, .6, {alpha:1, ease:'power3.inOut'},3);
+            if(!started){
+                if(Math.round(timer % 10) === 0){
+                    if(!doOnce){
+                        doOnce = true;
+                        by = 0;
+                        idx = Math.round(Math.random() * (objects.length-1));
+                    }
+                }
+                if(timer % 10 > 8){
+                    if(doOnce)
+                        doOnce = false;
+                    by += 1;
+                    Body.setVelocity(objects[idx], {x: 0, y: -by});
+                }
+                if(counter >= 60 * 1.5){
+                    Body.setVelocity(objects[Math.round(Math.random() * (objects.length-1))], {x: 0, y: -10});
+                    counter = 0;
+                }
             }
-        }
-
+        })
         Events.on(engine, 'afterUpdate', function() {
             engine.timing.timeScale += (timeScaleTarget - engine.timing.timeScale) * 0.05;
         });
@@ -534,7 +627,17 @@ const Dropping2d = (props) => {
                 // removeSpecificObject(2);
             }
             else{
+                if(!started){
+                    started = true;
+                    // for(let i=0; i<objects.length; i++){
+                    //     objects[i].collisionFilter.group = -1;
+                    // }
+                    removeAllObjects();
+                }
                 createObject();
+                pick.current.className = 'text active';
+                up.current.className = 'text active';
+                bg.current.className = 'active';
             }
         }
         
@@ -546,6 +649,10 @@ const Dropping2d = (props) => {
 
         initMatter();
         initPIXI();
+        createObject(true);
+        createObject(true);
+        createObject(true);
+        createObject(true);
 
         
         window.addEventListener('resize',(e)=>onResize(app));
@@ -560,8 +667,14 @@ const Dropping2d = (props) => {
     return <>
         {/* <LoadingScene /> */}
         <div style={{position:'fixed',top:0}}>
-            <div ref={sceneElem} id="scene" style={{backgroundColor:'#fac05d'}}></div>
-            <div ref={tempSceneElem} id="tempScene" style={{position:'absolute',top:0,pointerEvent:'none'}}></div>
+            <div ref={sceneElem} id="scene"></div>
+            <div ref={tempSceneElem} id="tempScene"></div>
+            <span ref={pick} id="pick" className="text">Pick</span>
+            <span ref={up} id="up" className="text">up</span>
+            <div id="ranking">
+
+            </div>
+            <div ref={bg} id="bg"></div>
         </div>
     </>
 }
